a1fe5d1ec416f2b61c3936e597b700dbdb2e01c4
Sverre Winkelmans <sverre.winkelmans@gmail.com>
2024-11-19 14:22:38 +0800

    Minor changes

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -1,3 +1,10 @@
 -- Keymaps are automatically loaded on the VeryLazy event
 -- Default keymaps that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/keymaps.lua
 -- Add any additional keymaps here
+--
+local map = vim.keymap.set
+
+map("t", "<C-d>", "<C-\\><C-n><C-w>l", {})
+map("n", "<leader>tt", "<cmd>ToggleTerm 1 direction=float<CR>", { desc = "Show floating terminal" })
+map("n", "<leader>tr", "<cmd>ToggleTerm 2 direction=vertical size=80<CR>", { desc = "Show terminal right side" })
+map("n", "<leader>tb", "<cmd>ToggleTerm 3 direction=horizontal <CR>", { desc = "Show terminal at the bottom" })
--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -7,3 +7,5 @@ vim.opt.relativenumber = true
 vim.opt.tabstop = 4 -- Number of visual spaces per TAB
 vim.opt.shiftwidth = 4 -- Spaces to use for each step of (auto)indent
 vim.opt.expandtab = true -- Convert tabs to spaces
+
+vim.g.terminal_emulator = "kitty"
